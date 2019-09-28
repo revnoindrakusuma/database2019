04319c25398c87e0a93ca851d6ef5bb285123716
Aulia Alfina Rahmadanti <auliaalfina64@gmail.com>
2019-09-28 15:50:33 +0700

    Add files via upload

---
+++ 20180801369_fk.sql
@@ -0,0 +1,27 @@
+relasi pengampu ('nidn','kodematkul')
+
+select * from 25_PENGAMPU
+
+INSERT INTO 25_PENGAMPU
+VALUES('6776','CR101')
+
+INSERT INTO 25_PENGAMPU
+VALUES('6777','CR102')
+
+INSERT INTO 25_PENGAMPU
+VALUES('6778','ESA201')
+
+***************************************
+
+relasi krs ('nim','kodematkul')
+
+select * from 25_KRS
+
+INSERT INTO 25_KRS
+VALUES('20180801369','CR101')
+
+INSERT INTO 25_KRS
+VALUES('20180801370','CR102')
+
+INSERT INTO 25_KRS
+VALUES('20180801371','ESA201')
