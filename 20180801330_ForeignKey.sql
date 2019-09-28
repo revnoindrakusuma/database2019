225afc0b216c50ffe4d8da228496bca2a9d102a6
Yulius Daniel <53181116+Yulius888@users.noreply.github.com>
2019-09-28 19:16:47 +0700

    Add files via upload

---
+++ 20180801330_ForeignKey.sql
@@ -0,0 +1,39 @@
+select * from T_PENGAMPU
+
+insert into T_PENGAMPU
+values('4876', 'CCP210')
+insert into T_PENGAMPU
+values('4877', 'CCR210')
+insert into T_PENGAMPU
+values('4880', 'ESA148')
+insert into T_PENGAMPU
+values('4878', 'ESA101')
+insert into T_PENGAMPU
+values('4881', 'CCB210')
+
+select * from T_WALI
+
+insert into T_WALI
+values('4878', '20180801331')
+insert into T_WALI
+values('4878', '20180801332')
+insert into T_WALI
+values('4880', '20180801333')
+insert into T_WALI
+values('4881', '20180801334')
+insert into T_WALI
+values('4881', '20180801336')
+
+select * from T_KRS
+
+insert into T_KRS
+values('20180801331', 'CCB210')
+insert into T_KRS
+values('20180801331', 'ESA148')
+insert into T_KRS
+values('20180801332', 'ESA101')
+insert into T_KRS
+values('20180801333', 'CCB210')
+insert into T_KRS
+values('20180801333', 'ESA148')
+
